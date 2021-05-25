e73288f0e46450ab0f79c434d450cb5f6044d9f8
ghostrib <sh4rkbot@gmail.com>
2021-05-25 11:34:05 -0500

    add basic server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+require('./routes')(app);
+
+app.listen(PORT);
